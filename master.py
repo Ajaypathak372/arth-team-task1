8c50e9c944ab13d7572fc91a68b6b79307fd5f5a
Ajaypathak372 <63955239+Ajaypathak372@users.noreply.github.com>
2020-11-09 20:43:50 +0530

    Create master.py

---
+++ master.py
@@ -0,0 +1,24 @@
+import os
+nn_ip = input("Enter the IP of the Hadoop master :- ")
+    os.system("""
+    sudo git clone --quiet https://github.com/Ajaypathak372/arth-team-task1.git > /dev/null
+    sudo cd arth-team-task1
+    bash hadoop
+    """)
+    nn_dir = input("Enter the directory name for hadoop master :-  ")
+    fin = open("hdfs-site.xml", "rt")
+    fout = open("/etc/hadoop/hdfs-site.xml", "wt")
+    for line in fin:
+        fout.write(line.replace('path', '{}'.format(nn_dir)))
+    fin.close()
+    fout.close()
+
+    fin = open("core-site.xml", "rt")
+    fout = open("/etc/hadoop/core-site.xml", "wt")
+    for line in fin:
+        fout.write(line.replace('ip', '{}'.format(nn_ip)))
+    fin.close()
+    fout.close()
+
+    os.system("hadoop-daemon.sh start namenode")
+    print("Hadoop master(NameNode) Setup Successfully")
